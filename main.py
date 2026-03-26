eec1a4e0c5884189c7e52033054603c6165faa03
Ryan Ramdehol <ravinashwork@gmail.com>
2026-03-26 15:19:35 -0400

    initialize Turle module components and panda module to access CSV data and set background image of screen object to the states gif

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
+# IMPORTS
+import pandas as pd
+from turtle import Turtle, Screen
+import time
+# CONSTANTS
+DEFAULT_SCREEN_WIDTH = 750
+DEFAULT_SCREEN_HEIGHT = 450
+DEFAULT_SCREEN_COLOR = "white"
+DEFAULT_SCREEN_TITLE = "US_States_Game"
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Initialize Screen Object
+screen = Screen()
+screen.setup(DEFAULT_SCREEN_WIDTH, DEFAULT_SCREEN_HEIGHT)
+screen.bgcolor(DEFAULT_SCREEN_COLOR)
+screen.title(DEFAULT_SCREEN_TITLE)
+screen.bgpic("blank_states_img.gif")
+screen.tracer(0)
 
+# Pandas Read CSV
+us_states = pd.read_csv('50_states.csv')
+us_states_list = us_states['state'].values.tolist()
+print(us_states_list)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# Initialize Game
+game_on = True
+while game_on:
+    screen.update()
+    time.sleep(0.1)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Screen exit on click
+screen.exitonclick()
